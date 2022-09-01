0596dd0b44db7fd4b64661db11928c61e0fc0c88
lyj157175 <lyj157175@163.com>
2022-09-01 13:56:21 +0800

    commit

--- 笔记/test.py
+++ 笔记/test.py
@@ -1,3 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModelForMaskedLM.from_pretrained("bert-base-uncased")
+
+
