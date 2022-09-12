0b742ec7c01a62ed0410a6e00512b3924e4c27e8
lyj157175 <lyj157175@163.com>
2022-09-12 18:33:59 +0800

    commit

--- 笔记/script/003mysql连接&mysql语法.py
+++ 笔记/script/003mysql连接&mysql语法.py
@@ -4,7 +4,6 @@ import datetime
 
 
 
-
 db = pymysql.connect(
     host="172.18.128.127",
     port=3306,
@@ -21,22 +20,16 @@ cursor.execute('show databases')
 try:
     t1 = time.time()
     time_gap = ((datetime.datetime.now()) + datetime.timedelta(days=-26)).strftime("%Y-%m-%d %H:%M:%S")
-
     now = int(time.time())
-    # sql = "select id, title, content, publish_time from news_offline_2022 " \
-    #       "where publish_time > '{time}' and content like '{query}' order by publish_time".format(time=time_gap, query='%' + query + '%')
 
     sql = "select event_content, publish_time from event_extract_history " \
           "where publish_time > '{time}' and event_subject = '{query}' order by publish_time".format(time=str(now - 259200),
                                                                                                         query='xxx')
-
     cursor.execute(sql)
     mysql_results = cursor.fetchall()  ### fetchone(), fetchmany(), fetchall()
 
     for line in mysql_results:
-        print(line[0])
-        print(line[1])
-
+        print(line[0], line[1])
 
     print('数据长度', len(mysql_results))
     print('用时', time.time()-t1)
@@ -44,6 +37,17 @@ try:
 
 except:
     print ("Error: unable to fetch data_csv")
+
+
+##### pymysql完成插入操作
+# try:
+#     sql = "insert into lyj1(id, title, content) values(10, 'xxx', 'rrr')"
+#     cursor.execute(sql)
+#     db.commit()
+
+# except Exception as e:
+#     print(str(e))
+#     db.rollback()  #回滚事务
 
 
 cursor.close()
@@ -72,7 +76,46 @@ db.close()
 
 
 
-# ----------------------------- mysql语法 ----------------------------------------------
+################### docker创建mysql及连接 ####################################
+# 1.在lyj_mysql目录下：
+# mkdir -p /user/yuanie/lyj_mysql/conf
+# mkdir -p /user/yuanie/lyj_mysql/data
+# mkdir -p /user/yuanie/lyj_mysql/logs
+
+# 2.在conf 目录下配置mysqld.cnf 文件
+# vim mysqld.cnf
+# # 输入以下内容，并保存退出
+# [client]
+# default-character-set=utf8
+# [mysqld]
+# character-set-server=utf8
+# collation-server=utf8_general_ci
+
+# 3.docker启动容器
+# docker run
+# --name lyj_mysql
+# -p 3306:3306
+# -v /user/yuanjie/lyj_mysql/conf:/etc/mysql/mysql.conf.d
+# -v /user/yuanjie/lyj_mysql/data:/var/lib/mysql
+# -v /user/yuanjie/lyj_mysql/logs:/logs
+# -e MYSQL_ROOT_PASSWORD=123456
+# -d mysql
+
+# 4.进入容器
+# docker exec -it lyj_mysql /bin/bash
+
+# 5.登录mysql
+# mysql -uroot -p
+# 输入password
+
+
+
+
+# ---------------------------------- mysql语法学习 ----------------------------------------------
+# 插入
+# insert into lyj1(id, title, content) values(3, 'insert111111', 'insert content');
+
+
 # join语法
 sql = 'select * from table_a, table_b where table_a.id = table_b.id'
 
---
+++ 笔记/script/pdf解析.py
@@ -0,0 +1,59 @@
+# encoding: utf-8
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTTextBoxHorizontal, LAParams
+from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+
+import requests
+import urllib3
+import io
+
+
+
+def PDFparse(url):
+    pdf_content = ''
+
+    with urllib3.PoolManager() as http:
+        r = http.request('GET', url)
+
+        with io.BytesIO(r.data) as fp:
+            # fp = open(path, 'rb')
+            praser = PDFParser(fp)
+            doc = PDFDocument()
+            praser.set_document(doc)
+            doc.set_parser(praser)
+            doc.initialize()
+
+            if not doc.is_extractable:
+                raise PDFTextExtractionNotAllowed
+            else:
+                rsrcmgr = PDFResourceManager()
+                laparams = LAParams()
+                device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+                interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+                # 循环遍历列表，每次处理一个page的内容
+                for page in doc.get_pages():  # doc.get_pages() 获取page列表
+                    # 利用解释器的process_page()方法解析读取单独页数
+                    interpreter.process_page(page)
+                    layout = device.get_result()
+                    for x in layout:
+                        if (isinstance(x, LTTextBoxHorizontal)):
+                            line = x.get_text()
+                            pdf_content += line
+                # print(pdf_content)
+    return pdf_content
+
+
+
+
+
+
+if __name__ == '__main__':
+    # path = "../公告test.pdf"
+    # fp = open(path, 'rb').read()  # 读取二进制
+    url = "http://disc.static.szse.cn/download/disc/disk03/finalpage/2022-09-09/d09a4fa8-cc54-45fe-858c-769ba0b56504.PDF"
+    pdf_res = PDFparse(url)
+    print(pdf_res)
+
